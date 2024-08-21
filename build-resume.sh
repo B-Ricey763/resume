f5ced99cab63567fd7ea851a4fd8770c415bee8e
B-Ricey763 <bhanna30@gatech.edu>
2024-08-21 09:59:26 -0400

    Update build script to compile PNG output

--- build-resume.sh
+++ build-resume.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 typst compile resume.typ ~/projects/portfolio/public/resume.png
+typst compile resume.typ hanna-bryce-resume.png
 typst compile resume.typ hanna-bryce-resume.pdf
