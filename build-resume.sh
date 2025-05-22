cb8247dc6e97e004126b7397e904dbdcf7468101
B-Ricey763 <bhanna30@gatech.edu>
2025-05-21 21:19:09 -0400

    Add initial project drafts and embedded resume version

--- build-resume.sh
+++ build-resume.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-typst compile resume.typ ~/projects/portfolio/public/resume.png
+typst compile resume_embedded.typ ~/projects/portfolio/public/resume.png
 typst compile resume.typ hanna-bryce-resume.png
 typst compile resume.typ hanna-bryce-resume.pdf
+typst compile resume_embedded.typ hanna-bryce-resume.pdf
