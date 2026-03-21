82a6a2d58079037b6351d837fb71a89fc273146d
B-Ricey763 <bhanna30@gatech.edu>
2026-03-21 14:24:34 -0700

    Add automatic git commit to resume build script

--- build-resume.sh
+++ build-resume.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Compile resume outputs
 typst compile resume.typ ~/projects/portfolio/public/resume.png
 typst compile resume.typ hanna-bryce-resume.png
 typst compile resume.typ hanna-bryce-resume.pdf
+
+# Automatically commit the build changes
+DATE=$(date +%Y-%m-%d)
+git add hanna-bryce-resume.png hanna-bryce-resume.pdf ~/projects/portfolio/public/resume.png
+git commit -m "chore: build resume [$DATE]"
